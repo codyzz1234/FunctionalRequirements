b63b7065758d8c1954b0fa9a77781919a0b5ffdf
Cody <kenleybeast1@gmail.com>
2022-06-27 00:45:16 +0800

    Added Passengers CSV file and class

---
+++ Airline Reservation System/Passengers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline_Reservation_System
+{
+  
+    internal class Passengers
+    {
+        public void addNewPassenger()
+        {
+
+        }
+    }
+
+    class passengerInfo
+    {
+        String firstName { get;set;}
+        String lastName { get;set;}
+        String birthDate { get;set;}
+        String age { get;set;}
+    }
+
+}
